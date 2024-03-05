dd7087041b6a1361625bfbb00e38aa377a8f3b35
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-05 13:58:48 +0100

    Add custom theme to the app

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/MainActivity.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/MainActivity.kt
@@ -1,12 +1,11 @@
 package es.udc.apm.swimchrono
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import es.udc.apm.swimchrono.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,12 +23,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 }
