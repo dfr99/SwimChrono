ed2597f84e2764f43fabfe0d4b2b8499fb795bd1
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-06 12:25:42 +0100

    Fix bug and styles location

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/MainActivity.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/MainActivity.kt
@@ -3,7 +3,6 @@ package es.udc.apm.swimchrono
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import es.udc.apm.swimchrono.databinding.ActivityMainBinding
@@ -21,14 +20,7 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
-            )
-        )
         navView.setupWithNavController(navController)
 
 
