784d17f55caf93efbdb9f1972d6e8a6f614ad284
a.vaquero <a.vaquero@udc.es>
2024-03-06 01:15:22 +0100

    Nuevo botón creado

---
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileFragment.kt
@@ -0,0 +1,46 @@
+package es.udc.apm.swimchrono.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import es.udc.apm.swimchrono.databinding.FragmentHomeBinding
+import es.udc.apm.swimchrono.databinding.FragmentProfileBinding
+import es.udc.apm.swimchrono.ui.home.HomeViewModel
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
