aed8ff0f9454b61d8e41f1183583d8bebe3869bd
AlejandroRVaquero <72246581+AlejandroRVaquero@users.noreply.github.com>
2024-03-07 17:35:01 +0100

    3 layouts creados #28 (#34)
    Closes #28 Closes #34

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileViewModel.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment xd"
+        value = "Profile Fragment"
     }
     val text: LiveData<String> = _text
 }
