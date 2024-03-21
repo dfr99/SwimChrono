a4dcac8a2213772d7587862b37c31156bf43a3b0
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-21 16:52:55 +0100

    Creación del comportamiento del DASHBOARD (#49)
    * Add onClick on tournaments. Fix some styles.
    
    * Refactor change names

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/tournaments/TournamentInfoViewModel.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/tournaments/TournamentInfoViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class TournamentsViewModel : ViewModel() {
+class TournamentInfoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
