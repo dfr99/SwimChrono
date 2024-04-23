aafb61865cd12e528d8f17019b07470e92a10aa7
a.vaquero <a.vaquero@udc.es>
2024-04-23 22:50:51 +0200

    app/GH-77: Bug del logueo

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileMyQRActivity.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileMyQRActivity.kt
@@ -7,15 +7,30 @@ import androidx.appcompat.app.AppCompatActivity
 import es.udc.apm.swimchrono.R
 
 class ProfileMyQRActivity : AppCompatActivity() {
+
+
+
+    // Inicializar variable SharedPreference??
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_myqractivity)
 
         val buttonExit = findViewById<ImageView>(R.id.ivBackButton)
 
+        // Sacar el sharedPreference
+
         buttonExit.setOnClickListener {
             Toast.makeText(this, "exit", Toast.LENGTH_SHORT).show()
             finish()
         }
     }
+
+
+
+
+
+
 }
