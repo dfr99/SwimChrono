954bf01edf5dd0a2d741407d65b144f6c6b57e90
a.vaquero <a.vaquero@udc.es>
2024-04-25 01:41:20 +0200

    app/GH-77: Implementación My QR correcta. Toca añadir el lector para los árbitros

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileMyQRActivity.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileMyQRActivity.kt
@@ -1,17 +1,20 @@
 package es.udc.apm.swimchrono.ui.profile
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import es.udc.apm.swimchrono.R
 
 class ProfileMyQRActivity : AppCompatActivity() {
 
 
-
-    // Inicializar variable SharedPreference??
-
+    private lateinit var sharedPreferences: SharedPreferences // Declarar la variable SharedPreferences
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +22,45 @@ class ProfileMyQRActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile_myqractivity)
 
         val buttonExit = findViewById<ImageView>(R.id.ivBackButton)
+        var ivCodigoQR: ImageView = findViewById(R.id.ivCodigoQR)
 
-        // Sacar el sharedPreference
+        sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE)
 
+
+        val userId = sharedPreferences.getString("userId", "null")
+
+        // Variables de prueba de funcionamiento
+        //val userId = "1234"
+        //Toast.makeText(this, "Bienvenido $userId", Toast.LENGTH_SHORT).show()
+
+        // Generación del QR
+        try {
+            var barcodeEncoder: BarcodeEncoder = BarcodeEncoder()
+            var bitmap: Bitmap = barcodeEncoder.encodeBitmap(
+                userId,
+                BarcodeFormat.QR_CODE, /*Creo que el tamaño no importa*/
+                300,
+                300
+            )
+
+            ivCodigoQR.setImageBitmap(bitmap)
+
+
+
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+
+
+
+
+        //Botón de salir
         buttonExit.setOnClickListener {
-            Toast.makeText(this, "exit", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "exit", Toast.LENGTH_SHORT).show()
             finish()
         }
+
+
     }
 
 
