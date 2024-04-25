99452c8b11c2d706ecb853b8d0d584192dd2b680
a.vaquero <a.vaquero@udc.es>
2024-04-25 14:16:01 +0200

    app/GH-77: Scan QR implementado pero sin correcto funcionamiento

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/referee/RefereeFragment.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/referee/RefereeFragment.kt
@@ -8,8 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.zxing.integration.android.IntentIntegrator
 import es.udc.apm.swimchrono.R
 import es.udc.apm.swimchrono.databinding.FragmentRefereeBinding
 import es.udc.apm.swimchrono.model.Race
@@ -35,6 +37,22 @@ class RefereeFragment : Fragment() {
             Log.d("BeginChronoClicked", "Go to chrono")
         }
 
+
+        // Button Scan QR
+
+        val btnScanQR: Button = view.findViewById(R.id.botonScanQR)
+        btnScanQR.setOnClickListener {
+
+
+            initQRScanner()
+
+
+        }
+
+
+
+
+
         val races = arrayOf (
             Race(id = 1, swimmer = "David", club = "SAL", race = "100 medley", heat = 6, lane = 3, hour = Date()),
         )
@@ -52,4 +70,47 @@ class RefereeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
+
+    // Scan QR functions
+
+    /**NO FUNCIONA**/
+    private fun initQRScanner() {
+        //IntentIntegrator(this).initiateScan() //DEPRECATED
+        val funciona = false
+        if (funciona) {
+            val integrator = IntentIntegrator(requireActivity())
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // Establecer el tipo de codigo de barras
+            integrator.setPrompt("Acerque la cámara al código QR")
+            integrator.setTorchEnabled(false) // Encender flash
+            integrator.setBeepEnabled(true) // Sonido de confirmacion de codigo escaneado
+            integrator.initiateScan()
+        } else {
+            Toast.makeText(requireContext(), "BOTÓN EN REPARACIÓN: Para escanear accede a START", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //Este método se le llama cada vez que vuelve de un activity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+            if(result.contents == null){
+                Toast.makeText(requireContext(), "Cancelado", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(requireContext(), "El valor escaneado es: ${result.contents}", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(requireContext(), "Exception?", Toast.LENGTH_SHORT).show()
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        Toast.makeText(requireContext(), "YA FUNCIONA", Toast.LENGTH_SHORT).show()
+
+    }
+
+
+
+
 }
--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/referee/TimerActivity.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/referee/TimerActivity.kt
@@ -1,5 +1,6 @@
 package es.udc.apm.swimchrono.ui.referee
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.widget.Chronometer
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 import es.udc.apm.swimchrono.R
 
 class TimerActivity : AppCompatActivity() {
@@ -17,6 +19,10 @@ class TimerActivity : AppCompatActivity() {
     private lateinit var lapTimesText: TextView
     private var lapNumber = 1
 
+    /** Eliminar esta variable si el scan se hace en otro sitio**/
+    // Variable para poder activar el crono una vez escaneado
+    private var enable_chronno = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_referee_start)
@@ -24,6 +30,9 @@ class TimerActivity : AppCompatActivity() {
         val buttonExit = findViewById<ImageView>(R.id.ivBackButton)
         val chronometer = findViewById<Chronometer>(R.id.chronometer)
         val startStopButton = findViewById<Button>(R.id.startStopButton)
+
+        /** Eliminar cuando se arregle el Scan en el fragment**/
+        val auxScanButton = findViewById<Button>(R.id.botonScanQRaux)
 
         buttonExit.setOnClickListener {
             Toast.makeText(this, "exit", Toast.LENGTH_SHORT).show()
@@ -38,32 +47,79 @@ class TimerActivity : AppCompatActivity() {
             }
             toggleStartStop(chronometer, startStopButton)
         }
+
+        /** Eliminar cuando se arregle el Scan en el fragment o se escanee en otro sitio**/
+        auxScanButton.setOnClickListener {
+            initQRScanner()
+        }
+
+
     }
 
     private fun toggleStartStop(chronometer: Chronometer, startStopButton: Button) {
-        if (isRunning) {
-            stopChronometer(chronometer)
-            startStopButton.text = getString(R.string.start)
-            startStopButton.setBackgroundColor(Color.argb(255,9,135,151))
-        } else {
-            startChronometer(chronometer)
-            startStopButton.text = getString(R.string.stop)
-            startStopButton.setBackgroundColor(Color.argb(255,255,0,0))
-        }
 
-        isRunning = !isRunning
+        if (enable_chronno) {
+            /** Eliminar este if si el scan se hace en otro sitio**/
+            if (isRunning) {
+                stopChronometer(chronometer)
+                startStopButton.text = getString(R.string.start)
+                startStopButton.setBackgroundColor(Color.argb(255, 9, 135, 151))
+            } else {
+                startChronometer(chronometer)
+                startStopButton.text = getString(R.string.stop)
+                startStopButton.setBackgroundColor(Color.argb(255, 255, 0, 0))
+            }
+
+            isRunning = !isRunning
+        } else {
+            Toast.makeText(this, "Escanea el QR del competidor", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun startChronometer(chronometer: Chronometer) {
-        if (!isRunning) {
-            chronometer.base = SystemClock.elapsedRealtime()
-            chronometer.start()
+        if (enable_chronno) { /** Eliminar este if si el scan se hace en otro sitio**/
+            if (!isRunning) {
+                chronometer.base = SystemClock.elapsedRealtime()
+                chronometer.start()
+            }
         }
     }
 
     private fun stopChronometer(chronometer: Chronometer) {
         if (isRunning) {
             chronometer.stop()
+        }
+    }
+
+
+
+    /** Eliminar cuando se arregle el Scan en el fragment**/
+    // Scan QR functions
+    private fun initQRScanner() {
+        //IntentIntegrator(this).initiateScan() //DEPRECATED
+
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // Establecer el tipo de codigo de barras
+        integrator.setPrompt("Acerque la cámara al código QR")
+        integrator.setTorchEnabled(false) // Encender flash
+        integrator.setBeepEnabled(true) // Sonido de confirmacion de codigo escaneado
+        integrator.initiateScan()
+    }
+
+    //Este método se le llama cada vez que vuelve de un activity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null){
+            if(result.contents == null){
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }else{
+                //Toast.makeText(this, "El valor escaneado es: ${result.contents}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Puede iniciar el crono", Toast.LENGTH_SHORT).show()
+                enable_chronno = true
+            }
+        }else{
+            Toast.makeText(this, "Exception?", Toast.LENGTH_SHORT).show()
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
