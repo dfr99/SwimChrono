632d5d776221a219954b835aec39b60045e2d815
a.vaquero <a.vaquero@udc.es>
2024-05-22 03:20:31 +0200

    app/GH-103: Intento de perfil personal fallido

--- app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileInfoActivity.kt
+++ app/SwimChrono/app/src/main/java/es/udc/apm/swimchrono/ui/profile/ProfileInfoActivity.kt
@@ -1,17 +1,48 @@
 package es.udc.apm.swimchrono.ui.profile
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import es.udc.apm.swimchrono.R
 
 class ProfileInfoActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_info)
 
+
+
         val buttonExit = findViewById<ImageView>(R.id.ivBackButton)
+
+
+
+        val nameUser = findViewById<TextView>(R.id.tvName)
+        val surnameUser = findViewById<TextView>(R.id.tvSurname)
+        val telephoneNumber = findViewById<TextView>(R.id.tvNumber)
+        val rolUser = findViewById<TextView>(R.id.tvRol)
+        val nameClub = findViewById<TextView>(R.id.tvClub)
+
+
+
+        nameUser.setText("1")
+        surnameUser.setText("2")
+        telephoneNumber.setText("3")
+        rolUser.setText("4")
+        nameClub.setText("5")
+
+
+
+
+
+
+
+
+
 
         buttonExit.setOnClickListener {
             Toast.makeText(this, "exit", Toast.LENGTH_SHORT).show()
